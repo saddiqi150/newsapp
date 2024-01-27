e8b2ace9fce00ca8f5611614a214ae554d569590
Sadiq.Ullah <Sadiq.Ullah@TEO-SADIQ1>
2024-01-27 20:50:48 +0500

    Page Loader Added

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner';
 
 export class News extends Component {
 
@@ -13,40 +14,47 @@ export class News extends Component {
     }
   }
  async componentDidMount(){
-    let url='https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=86c9d7772ae74f06b2cc847d0a116229&page=1&pageSize=6';
+    let url=`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=86c9d7772ae74f06b2cc847d0a116229&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true})
     let data= await fetch(url);
     let parseData=await data.json(data);
     console.log(parseData.articles);
-    this.setState({articles:parseData.articles, totalCount:parseData.totalResults});
+    this.setState({articles:parseData.articles, totalCount:parseData.totalResults, loading:false});
   }
   handleNextClick = async()=>{
     if(this.state.page +1 >Math.ceil(this.state.totalCount/6)){
 
     }
-    let url=`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=86c9d7772ae74f06b2cc847d0a116229&page=${this.state.page}&pageSize=6`;
+    let url=`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=86c9d7772ae74f06b2cc847d0a116229&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true})
     let data= await fetch(url);
     let parseData=await data.json(data);
     this.setState({
       page:this.state.page+1,
-      articles:parseData.articles
+      articles:parseData.articles,
+      loading:false
     })
   }
   handlePreviousClick = async()=>{
-      let url=`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=86c9d7772ae74f06b2cc847d0a116229&page=${this.state.page}&pageSize=6`;
-      let data= await fetch(url);
+    let url=`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=86c9d7772ae74f06b2cc847d0a116229&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true})  
+    let data= await fetch(url);
       let parseData=await data.json(data);
       this.setState({
         page:this.state.page-1,
-        articles:parseData.articles
+        articles:parseData.articles,
+        loading:false
       })
     
   }
     render() {
         return (
+       
             <div className='container my-3'>
-                <h2>SkyNews - Top Headlines </h2>
+                {this.state.loading && <Spinner />}
+                <h2 className='text-center' >SkyNews - Top Headlines </h2>
                 <div className='row'>
-                  {this.state.articles.map((element)=>{
+                  {!this.state.loading && this.state.articles.map((element)=>{
                     return   <div key={element.url} className='col-md-4'>
                     <NewsItem   title={element.title?element.title.slice(0,40):""} 
                     description={element.description?element.description.slice(0,80):""} 
@@ -56,7 +64,7 @@ export class News extends Component {
                   })}
                   <div className="container d-flex justify-content-between">
                       <button disabled={this.state.page<=1} type="button" onClick={this.handlePreviousClick} className="btn btn-dark">&laquo; Previous</button>
-                      <button type="button" onClick={this.handleNextClick} className="btn btn-dark">Next &raquo;</button>
+                      <button disabled={this.state.page +1 >Math.ceil(this.state.totalCount/this.props.pageSize)} type="button" onClick={this.handleNextClick} className="btn btn-dark">Next &raquo;</button>
                   </div>
                 </div>
             </div>
---
+++ src/components/Spinner.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Loader from './Loader.gif'
+export default function Spinner() {
+  return (
+    <div className='text-center'>
+      <img src={Loader} alt='Loader '></img>
+    </div>
+  )
+}
